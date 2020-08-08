8ae9022c46f933aede64390e4b79ca2a1624e5a4
seanyusa <seanyusa@gmail.com>
2020-08-08 08:46:06 -0500

    use react community picker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Text, View, Image, TouchableWithoutFeedback, Animated, Picker, Platform, StyleSheet } from 'react-native'
+import { Picker } from '@react-native-community/picker'
+import { Text, View, Image, TouchableWithoutFeedback, Animated, Platform, StyleSheet } from 'react-native'
 import ToggleBox from 'react-native-togglebox'
 import styles from './styles.js'
 
